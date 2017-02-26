b478e5ec484471a4ff6461a73783a1dce7d76751
Liubomyr <Liubomyr.Kinash.MTR.2016@lpnu.ua>
2017-02-27 00:13:06 +0200

    Start tcp

--- tcp/src/main/java/Tcp/tcp/App.java
+++ tcp/src/main/java/Tcp/tcp/App.java
@@ -1,13 +1,64 @@
 package Tcp.tcp;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.io.*;
+import java.util.*;
+public class App {
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+   public static void main(String[] args) {
+      
+      final Socket clientSocket;
+      final BufferedReader in;
+      final PrintWriter out;
+      final Scanner sc = new Scanner(System.in);
+  
+      try {
+               clientSocket = new Socket("127.0.0.1",5000);
+   
+         
+         out = new PrintWriter(clientSocket.getOutputStream());
+        
+         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+   
+         Thread envoyer = new Thread(new Runnable() {
+             String msg;
+              @Override
+              public void run() {
+                while(true){
+                  msg = sc.nextLine();
+                  out.println(msg);
+                  out.flush();
+                }
+             }
+         });
+         envoyer.start();
+   
+        Thread recevoir = new Thread(new Runnable() {
+            String msg;
+            @Override
+            public void run() {
+               try {
+                 msg = in.readLine();
+                 while(msg!=null){
+                    System.out.println("Server : "+msg);
+                    msg = in.readLine();
+                 }
+                 System.out.println("Server desconnect");
+                 out.close();
+                 clientSocket.close();
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+            }
+        });
+        recevoir.start();
+   
+      } catch (IOException e) {
+           e.printStackTrace();
+      }
+  }
 }
